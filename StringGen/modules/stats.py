aa9a688974a0b5dd62093542a83d05f9fde879fb
ramowlf <ramodiscord26@gmail.com>
2024-06-30 05:22:25 +0300

    Update stats.py

--- StringGen/modules/stats.py
+++ StringGen/modules/stats.py
@@ -9,4 +9,4 @@ from StringGen.utils import get_served_users
 @Anony.on_message(filters.command(["stats", "users"]) & filters.user(OWNER_ID))
 async def get_stats(_, message: Message):
     users = len(await get_served_users())
-    await message.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {Anony.name} :\n\n {users} ᴜsᴇʀs")
+    await message.reply_text(f"» '𝑖𝑛 𝑚𝑒𝑣𝑐𝑢𝑡 𝑖𝑠𝑡𝑎𝑡𝑖𝑠𝑡𝑖𝑘𝑙𝑒𝑟𝑖 {Anony.name} :\n\n {users} 𝑘𝑢𝑙𝑙𝑎𝑛𝚤𝑐𝚤")
